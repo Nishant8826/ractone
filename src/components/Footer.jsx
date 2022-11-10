7e4b081f025eebb0de9320a0c4c4c881a5710b1d
Nishant Rathore <rnishant609@gmail.com>
2022-11-10 20:12:41 +0530

    react-one

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const footer = () => {
+  return <footer>
+    <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+    </div>
+    <div>
+        <h1>follow us</h1>
+        <div>
+            <a href="https://www.linkedin.com/in/nishant-rathore-253589238/" target={"blank"}>LinkedIn</a>
+            <a href="https://www.instagram.com/nishant_sw/" target={"blank"}>Instagram</a>
+            <a href="https://www.facebook.com/neel.kamal.509994/" target={"blank"}>Facebook</a>
+            <a href="https://github.com/Nishant8826" target={"blank"}>Github</a>
+        </div>
+    </div>
+  </footer>
+}
+
+export default footer
